acdd79da439994a84164aa5879f2f2d18f333545
Sherinsheri <fathimasherinc2003@gmail.com>
2025-01-24 01:20:08 +0530

    newww

--- tracker/settings.py
+++ tracker/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tracker',
          'USER':'postgres',
-         'PASSWORD':'1234',
+         'PASSWORD':'sherin',
          'HOST':'localhost',
     }
 }
