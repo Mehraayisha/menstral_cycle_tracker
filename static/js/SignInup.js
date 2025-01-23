48c0ab1ce5e7f96bd7b9d94fcfad406683c7e3a8
Mehraayisha <https://github.com/Mehraayisha>
2025-01-23 20:39:01 +0530

    mehra

--- static/js/SignInup.js
+++ static/js/SignInup.js
@@ -1,23 +1,11 @@
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-        // Focus on the first input of the form when the page loads
-        if (document.querySelector('.form-container .sign-up input[name="username"]:focus') === null) {
-            document.querySelector('.form-container .sign-up input[name="username"]').focus();
-        }
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
 
-        // Get the container and buttons
-        const container = document.getElementById('container');
-        const registerBtn = document.getElementById('register');
-        const loginBtn = document.getElementById('login');
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
 
-        // When the Register button is clicked, add the 'active' class to the container
-        registerBtn.addEventListener('click', () => {
-            container.classList.add("active");
-        });
-
-        // When the Login button is clicked, remove the 'active' class from the container
-        loginBtn.addEventListener('click', () => {
-            container.classList.remove("active");
-        });
-    });
-</script>
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
